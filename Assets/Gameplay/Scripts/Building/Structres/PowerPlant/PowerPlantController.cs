b6bd1d36c8f66fb8a2cad40e9b98851ee5535435
arikayvaz <54839802+arikayvaz@users.noreply.github.com>
2024-02-07 20:54:14 +0300

    Add PowerPlant Building

---
+++ Assets/Gameplay/Scripts/Building/Structres/PowerPlant/PowerPlantController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class PowerPlantController : BuildingControllerBase
+    {
+        public override BuildingTypes BuildingType => BuildingTypes.PowerPlant;
+    }
+}
